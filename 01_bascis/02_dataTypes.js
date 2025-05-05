ddc7dfa6c015af07e226efc7352b382b711cbdb5
amanjha10 <109917105+amanjha10@users.noreply.github.com>
2025-05-05 16:38:07 +0000

    datatypes

---
+++ 01_bascis/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(" 3 + 3 ")// we are using nodejs , not browser
+
+console.log( 3 + 3 ); console.log("Hello World") // Less readable
+
+let name = "aman"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// Boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "Aman");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
+
